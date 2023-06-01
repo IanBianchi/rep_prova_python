583aaad7d1e6ad3954b84a1f724f43c271d92fa7
IanBianchi <134499952+IanBianchi@users.noreply.github.com>
2023-06-01 11:02:56 +0000

    Aggiunta Jupyter

--- prova.py
+++ prova.py
@@ -1,12 +1,16 @@
 # Prova delle funzionalità di Git
 
+# %%
+# Shift + Enter per fare run di una cella
 print('Questo è un file python')
+
+# %%
 
 a = 2
 b = 3
 
-print('a =', a)
-print('b =', b)
-print('a+b =', a + b)
+print("a =", a)
+print("b =", b)
+print("a+b =", a + b)
 
-print('return 0')
+print("return 0")
